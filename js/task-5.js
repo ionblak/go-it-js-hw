9a0304aa593d40b1311a0445f8b8e63350e5b88a
Igor Ionasku <ionaigor10@gmail.com>
2020-09-30 20:56:38 +0300

    Update task-5.js

--- js/task-5.js
+++ js/task-5.js
@@ -1,24 +1,48 @@
 class Car {
-  constructor() {
-    this.car = {
-      speed: 0,
-      price,
-      maxSpeed,
-      isOn: false,
-      distance: 0,
-    };
-  }
-  getSpecs(car) {
-    return `'maxSpeed: ${maxSpeed}, speed: ${speed}, isOn: ${isOn}, distance: ${distance}, price: ${price}'`;
+  static getSpecs(car) {
+    return `maxSpeed: ${car.maxSpeed}, speed: ${car.speed}, isOn: ${car.isOn}, distance: ${car.distance}, price: ${car._price}`;
   }
 
-  get price() {}
-  set price(value) {}
-  turnOn() {}
-  turnOff() {}
-  accelerate(value) {}
-  decelerate(value) {}
-  drive(hours) {}
+  constructor({ speed = 0, price, maxSpeed, isOn = false, distance = 0 } = {}) {
+    this.speed = speed;
+    this._price = price;
+    this.maxSpeed = maxSpeed;
+    this.isOn = isOn;
+    this.distance = distance;
+  }
+
+  get price() {
+    return this._price;
+  }
+  set price(value) {
+    this._price = value;
+  }
+  turnOn() {
+    this.isOn = true;
+  }
+  turnOff() {
+    this.isOn = false;
+    this.speed = 0;
+  }
+  accelerate(value) {
+    if (value < this.maxSpeed) {
+      this.speed += value;
+    } else {
+      this.speed = this.maxSpeed;
+    }
+  }
+  decelerate(value) {
+    if (value > 0 && value < this.speed) {
+      this.speed -= value;
+    } else {
+      this.speed = 0;
+    }
+  }
+  drive(hours) {
+    if (this.isOn === true) {
+      this.distance += hours * this.speed;
+    }
+  }
 }
 
 const mustang = new Car({ maxSpeed: 200, price: 2000 });
@@ -27,14 +51,14 @@ mustang.accelerate(50);
 mustang.drive(2);
 
 console.log(Car.getSpecs(mustang));
-// 'maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000'
+// // 'maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000'
 
 mustang.decelerate(20);
 mustang.drive(1);
 mustang.turnOff();
 
 console.log(Car.getSpecs(mustang));
-// 'maxSpeed: 200, speed: 0, isOn: false, distance: 130, price: 2000'
+// // 'maxSpeed: 200, speed: 0, isOn: false, distance: 130, price: 2000'
 
 console.log(mustang.price); // 2000
 mustang.price = 4000;
